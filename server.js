866c5dec43cb93c2cc05c80ad0b25f7432f3ba26
Steven Li <sli737@uwo.ca>
2017-11-05 23:50:07 +0000

    Added the middleware for requests and added a model for 'Bear'

--- server.js
+++ server.js
@@ -7,6 +7,10 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/bears');
+
+var Bear     = require('./app/models/bear');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -19,9 +23,17 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
+    //console.log(req);
 });
 
 // more routes for our API will happen here
