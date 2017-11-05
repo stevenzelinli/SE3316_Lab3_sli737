056dd937a4741f17c8a9535d67eef05c0e110ffb
Steven Li <sli737@uwo.ca>
2017-11-05 23:58:59 +0000

    Created bears in the database through POST requests

--- server.js
+++ server.js
@@ -38,6 +38,24 @@ router.get('/', function(req, res) {
 
 // more routes for our API will happen here
 
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/bears')
+    // create a bear (accessed at POST http://https://lab3-sli737-wulios.c9users.io:8080/api/bears)
+    .post(function(req, res) {
+
+        var bear = new Bear();      // create a new instance of the Bear model
+        bear.name = req.body.name;  // set the bears name (comes from the request)
+
+        // save the bear and check for errors
+        bear.save(function(err) {
+            if (err)
+                res.send(err);
+            res.json({ message: 'Bear created!' });
+        });
+
+    });
+    
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
