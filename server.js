524c166c1b57b5eeaf982245ab7642709c4799bc
Steven Li <sli737@uwo.ca>
2017-11-06 00:27:53 +0000

    finished deleting and updating bears routes, finished part1

--- server.js
+++ server.js
@@ -73,6 +73,38 @@ router.route('/bears/:bear_id')
                 res.send(err);
             res.json(bear);
         });
+    })
+    // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
+    .put(function(req, res) {
+
+        // use our bear model to find the bear we want
+        Bear.findById(req.params.bear_id, function(err, bear) {
+
+            if (err)
+                res.send(err);
+
+            bear.name = req.body.name;  // update the bears info
+
+            // save the bear
+            bear.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Bear updated!' });
+            });
+
+        });
+    })
+    // delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
+    .delete(function(req, res) {
+        Bear.remove({
+            _id: req.params.bear_id
+        }, function(err, bear) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
     });
     
 // REGISTER OUR ROUTES -------------------------------
