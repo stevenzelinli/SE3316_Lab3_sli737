57fd4df3d3f2d67a0a0b24b02f0bc45b6a7607dd
Steven Li <sli737@uwo.ca>
2017-11-06 01:17:26 +0000

    Finished routing getting specific messages by id and deleting messages by id, also made tests for each routing which all passed

--- server.js
+++ server.js
@@ -4,7 +4,6 @@
 // =============================================================================
 
 // call the packages we need
-var date = new Date();
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
@@ -44,14 +43,14 @@ router.get('/', function(req, res) {
 router.route('/messages')
     // create message
     .post(function(req, res) {
-        var message = new Message();      // create a new instance of the Bear model
-        message.content = req.body.content;  // set the bears name (comes from the request)
-        message.timestamp = date.toUTCString();
-        // save the bear and check for errors
+        var message = new Message();      
+        message.content = req.body.content;  // set message text
+        message.timestamp = new Date().getTime();
+        // save the timestamp and check for errors
         message.save(function(err) {
             if (err)
                 res.send(err);
-            res.json({ message: 'Message created!' });
+            res.json({ message: 'Message delivered!' });
         });
 
     })
@@ -60,46 +59,24 @@ router.route('/messages')
         Message.find(function(err, messages) {
             if (err)
                 res.send(err);
-
             res.json(messages);
         });
     });
 
-router.route('/bears/:bear_id')
+router.route('/messages/:message_id')
 
     // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
     .get(function(req, res) {
-        Bear.findById(req.params.bear_id, function(err, bear) {
+        Message.findById(req.params.message_id, function(err, message) {
             if (err)
                 res.send(err);
-            res.json(bear);
-        });
-    })
-    // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
-    .put(function(req, res) {
-
-        // use our bear model to find the bear we want
-        Bear.findById(req.params.bear_id, function(err, bear) {
-
-            if (err)
-                res.send(err);
-
-            bear.name = req.body.name;  // update the bears info
-
-            // save the bear
-            bear.save(function(err) {
-                if (err)
-                    res.send(err);
-
-                res.json({ message: 'Bear updated!' });
-            });
-
+            res.json(message);
         });
     })
     // delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
     .delete(function(req, res) {
-        Bear.remove({
-            _id: req.params.bear_id
+        Message.remove({
+            _id: req.params.message_id
         }, function(err, bear) {
             if (err)
                 res.send(err);
