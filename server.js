cba323fca171691d3c2971bda4bfe6b550f2e706
Steven Li <sli737@uwo.ca>
2017-11-06 00:17:46 +0000

    made route for getting all bears and a route for a single bear using the 'bear_id'

--- server.js
+++ server.js
@@ -43,7 +43,6 @@ router.get('/', function(req, res) {
 router.route('/bears')
     // create a bear (accessed at POST http://https://lab3-sli737-wulios.c9users.io:8080/api/bears)
     .post(function(req, res) {
-
         var bear = new Bear();      // create a new instance of the Bear model
         bear.name = req.body.name;  // set the bears name (comes from the request)
 
@@ -54,6 +53,26 @@ router.route('/bears')
             res.json({ message: 'Bear created!' });
         });
 
+    })
+    
+    .get(function(req, res) {
+        Bear.find(function(err, bears) {
+            if (err)
+                res.send(err);
+
+            res.json(bears);
+        });
+    });
+
+router.route('/bears/:bear_id')
+
+    // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+    .get(function(req, res) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
+            if (err)
+                res.send(err);
+            res.json(bear);
+        });
     });
     
 // REGISTER OUR ROUTES -------------------------------
